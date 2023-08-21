f684a4fbb6552e881f636553a332e4392bd7bdce
Mo Khosh <mskhoshnazar@gmail.com>
2023-08-21 13:33:47 +0330

    passing variables to views

--- laravel-view-blade/routes/web.php
+++ laravel-view-blade/routes/web.php
@@ -6,3 +6,9 @@ Route::view('/', 'welcome');
 
 // you can simply create files called xxxxx.blade.php in /resources/views and use them in routes as xxxxx
 Route::view('/new-blade', 'new');
+
+// if your view needs a variable, you can pass variables to views with this syntax
+// the variable will be avialable in the view by the name you specify as the array key
+Route::get('/hello/{name}', function ($name) {
+    return view('hello', ['name' => $name]);
+});
