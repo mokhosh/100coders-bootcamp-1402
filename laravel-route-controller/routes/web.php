4450755db77bb2c1317a5de2bf3d736bf6ffeaf8
Mo Khosh <mskhoshnazar@gmail.com>
2023-08-21 12:48:46 +0330

    comments on the routes/web.php file

--- laravel-route-controller/routes/web.php
+++ laravel-route-controller/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
+// import the Laravel Route class that helps us define routes
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
+// simple route handling requests that come to / (root)
+// handles the requests with a closure (unnamed function)
+// the closure returns a view
+// the view is located at /resources/views/welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
 });
