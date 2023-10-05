581b5b5a8013717b9f2e10b90480c81e84af6b32
Mo Khosh <mskhoshnazar@gmail.com>
2023-10-05 19:04:34 +0330

    add users to seeder

--- laravel-blog/database/seeders/DatabaseSeeder.php
+++ laravel-blog/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,20 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Mo Khosh',
+             'email' => 'mskhoshnazar@gmail.com',
+             'password' => Hash::make('1234567890'),
+             'title' => 'Mo Khosh',
+             'username' => 'mokhosh',
+         ]);
+
+         \App\Models\User::factory()->create([
+             'name' => 'Test User',
+             'email' => 'test@gmail.com',
+             'password' => Hash::make('1234567890'),
+             'title' => 'Test Blog',
+             'username' => 'test-blog',
+         ]);
     }
 }
