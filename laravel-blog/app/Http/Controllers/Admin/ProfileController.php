07a7fd8431cc740fd2c8ba4310dd98308a06e865
Mo Khosh <mskhoshnazar@gmail.com>
2023-10-22 16:03:49 +0330

    fix previous profile image not being deleted

--- laravel-blog/app/Http/Controllers/Admin/ProfileController.php
+++ laravel-blog/app/Http/Controllers/Admin/ProfileController.php
@@ -22,7 +22,7 @@ class ProfileController extends Controller
 
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $request->user()->fill($request->safe()->except('image'));
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
